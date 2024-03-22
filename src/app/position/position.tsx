93f02db41ab2d70bb5bcad8f28d3e6da93917ef4
alexcraig <alexcraig043@gmail.com>
2024-03-22 17:52:53 -0400

    block explorer links, tooltips

--- src/app/position/position.tsx
+++ src/app/position/position.tsx
@@ -12,10 +12,10 @@ import {
   roundedHealthFactor,
   formatAddress,
   getMarketSymbol,
-  getMarkets,
 } from "../_utils/textHandling";
 import { useRouter, useSearchParams } from "next/navigation";
-import { Loader } from "@mantine/core";
+import { Loader, Tooltip } from "@mantine/core";
+import Link from "next/link";
 
 import {
   PieChart,
@@ -517,48 +517,79 @@ export default function Position({
             <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
           </svg>
         </div>
-        <div className={styles.input}>
-          <span>Look Back:</span>
-          <select
-            value={lookBack}
-            onChange={(e) => {
-              setLookBack(e.target.value);
-              handleParameterChange("lookBack", e.target.value);
-              setPosition([]);
-            }}
-          >
-            <option value={"30"}>30 Days</option>
-            <option value={"90"}>90 Days</option>
-            <option value={"180"}>180 Days</option>
-          </select>
-        </div>
-        <div className={styles.input}>
-          <span>Look Forward:</span>
-          <select
-            value={lookForward}
-            onChange={(e) => {
-              setLookForward(e.target.value);
-            }}
-          >
-            <option value={"30"}>30 Days</option>
-            <option value={"90"}>90 Days</option>
-            <option value={"180"}>180 Days</option>
-          </select>
-        </div>
-        <div className={styles.input}>
-          <span>Cutoff:</span>
-          <select
-            value={probabilityCutoff}
-            onChange={(e) => {
-              setProbabilityCutoff(e.target.value);
-            }}
-          >
-            <option value={".05"}>5%</option>
-            <option value={".1"}>10%</option>
-            <option value={".2"}>20%</option>
-            <option value={".5"}>50%</option>
-          </select>
-        </div>
+        <Tooltip
+          label={
+            "The number of days to analyze the price action of tokens within the wallet's position."
+          }
+          position="bottom"
+          withArrow
+          classNames={{
+            tooltip: styles.tooltip,
+          }}
+        >
+          <div className={styles.input}>
+            <span>Look Back:</span>
+            <select
+              value={lookBack}
+              onChange={(e) => {
+                setLookBack(e.target.value);
+                handleParameterChange("lookBack", e.target.value);
+                setPosition([]);
+              }}
+            >
+              <option value={"30"}>30 Days</option>
+              <option value={"90"}>90 Days</option>
+              <option value={"180"}>180 Days</option>
+            </select>
+          </div>
+        </Tooltip>
+
+        <Tooltip
+          label={"The time horizon to analyze the probability of liquidation."}
+          position="bottom"
+          withArrow
+          classNames={{
+            tooltip: styles.tooltip,
+          }}
+        >
+          <div className={styles.input}>
+            <span>Look Forward:</span>
+            <select
+              value={lookForward}
+              onChange={(e) => {
+                setLookForward(e.target.value);
+              }}
+            >
+              <option value={"30"}>30 Days</option>
+              <option value={"90"}>90 Days</option>
+              <option value={"180"}>180 Days</option>
+            </select>
+          </div>
+        </Tooltip>
+
+        <Tooltip
+          label={"The probability cutoff to determine the days to liquidation."}
+          position="bottom"
+          withArrow
+          classNames={{
+            tooltip: styles.tooltip,
+          }}
+        >
+          <div className={styles.input}>
+            <span>Cutoff:</span>
+            <select
+              value={probabilityCutoff}
+              onChange={(e) => {
+                setProbabilityCutoff(e.target.value);
+              }}
+            >
+              <option value={".05"}>5%</option>
+              <option value={".1"}>10%</option>
+              <option value={".2"}>20%</option>
+              <option value={".5"}>50%</option>
+            </select>
+          </div>
+        </Tooltip>
       </div>
       {position.length === 0 && address.length > 0 && (
         <div className={styles.loader}>
@@ -623,7 +654,7 @@ export default function Position({
                       outerRadius={140}
                       fill="#8884d8"
                       paddingAngle={0}
-                      dataKey="balance"
+                      dataKey="value"
                       style={{ cursor: "pointer", outline: "none" }}
                     >
                       <Label
@@ -703,48 +734,62 @@ export default function Position({
                     />
                   </PieChart>
                 </ResponsiveContainer>
-                <div className={styles.positionInfo}>
-                  <div className={styles.protocol}>
-                    <Image
-                      src={
-                        `/protocolIcons/${protocol.protocol.name}.svg` ||
-                        defaultImg
-                      }
-                      alt={""}
-                      width={35}
-                      height={35}
-                    />
-                    {protocolToTitle(protocol.protocol.protocol)}
-                    <Image
-                      src={
-                        `/protocolTokenIcons/${protocol.protocol.chain}.svg` ||
-                        defaultImg
-                      }
-                      alt={""}
-                      width={35}
-                      height={35}
-                    />
-                  </div>
-                  <div className={styles.market}>{marketName} Market</div>
-                  <div
-                    className={getHealthFactorClass(
-                      Number(roundedHealthFactor(metadata.health_factor)),
-                      styles,
-                      false
-                    )}
-                  >
-                    <span>
-                      {Number(roundedHealthFactor(metadata.health_factor)) ===
-                      -1 ? (
-                        <svg viewBox="0 0 24 24">
-                          <path d="M18.6 6.62c-1.44 0-2.8.56-3.77 1.53L12 10.66 10.48 12h.01L7.8 14.39c-.64.64-1.49.99-2.4.99-1.87 0-3.39-1.51-3.39-3.38S3.53 8.62 5.4 8.62c.91 0 1.76.35 2.44 1.03l1.13 1 1.51-1.34L9.22 8.2C8.2 7.18 6.84 6.62 5.4 6.62 2.42 6.62 0 9.04 0 12s2.42 5.38 5.4 5.38c1.44 0 2.8-.56 3.77-1.53l2.83-2.5.01.01L13.52 12h-.01l2.69-2.39c.64-.64 1.49-.99 2.4-.99 1.87 0 3.39 1.51 3.39 3.38s-1.52 3.38-3.39 3.38c-.9 0-1.76-.35-2.44-1.03l-1.14-1.01-1.51 1.34 1.27 1.12c1.02 1.01 2.37 1.57 3.82 1.57 2.98 0 5.4-2.41 5.4-5.38s-2.42-5.37-5.4-5.37z"></path>
-                        </svg>
-                      ) : (
-                        roundedHealthFactor(metadata.health_factor)
+                <div className={styles.info}>
+                  <div className={styles.positionInfo}>
+                    <div className={styles.protocol}>
+                      <Image
+                        src={
+                          `/protocolIcons/${protocol.protocol.name}.svg` ||
+                          defaultImg
+                        }
+                        alt={""}
+                        width={35}
+                        height={35}
+                      />
+                      {protocolToTitle(protocol.protocol.protocol)}
+                      <Image
+                        src={
+                          `/protocolTokenIcons/${protocol.protocol.chain}.svg` ||
+                          defaultImg
+                        }
+                        alt={""}
+                        width={35}
+                        height={35}
+                      />
+                    </div>
+                    <div className={styles.market}>{marketName} Market</div>
+                    <div
+                      className={getHealthFactorClass(
+                        Number(roundedHealthFactor(metadata.health_factor)),
+                        styles,
+                        false
                       )}
-                    </span>
-                    {" Health Factor"}
+                    >
+                      <span>
+                        {Number(roundedHealthFactor(metadata.health_factor)) ===
+                        -1 ? (
+                          <svg viewBox="0 0 24 24">
+                            <path d="M18.6 6.62c-1.44 0-2.8.56-3.77 1.53L12 10.66 10.48 12h.01L7.8 14.39c-.64.64-1.49.99-2.4.99-1.87 0-3.39-1.51-3.39-3.38S3.53 8.62 5.4 8.62c.91 0 1.76.35 2.44 1.03l1.13 1 1.51-1.34L9.22 8.2C8.2 7.18 6.84 6.62 5.4 6.62 2.42 6.62 0 9.04 0 12s2.42 5.38 5.4 5.38c1.44 0 2.8-.56 3.77-1.53l2.83-2.5.01.01L13.52 12h-.01l2.69-2.39c.64-.64 1.49-.99 2.4-.99 1.87 0 3.39 1.51 3.39 3.38s-1.52 3.38-3.39 3.38c-.9 0-1.76-.35-2.44-1.03l-1.14-1.01-1.51 1.34 1.27 1.12c1.02 1.01 2.37 1.57 3.82 1.57 2.98 0 5.4-2.41 5.4-5.38s-2.42-5.37-5.4-5.37z"></path>
+                          </svg>
+                        ) : (
+                          roundedHealthFactor(metadata.health_factor)
+                        )}
+                      </span>
+                      {" Health Factor"}
+                    </div>
                   </div>
+                  <Link
+                    className={styles.explorer}
+                    href={`${protocol.protocol.explorer}/${address}`}
+                    target="_blank"
+                  >
+                    View on Explorer
+                    <svg x="0px" y="0px" viewBox="0 0 122.6 122.88">
+                      <g>
+                        <path d="M110.6,72.58c0-3.19,2.59-5.78,5.78-5.78c3.19,0,5.78,2.59,5.78,5.78v33.19c0,4.71-1.92,8.99-5.02,12.09 c-3.1,3.1-7.38,5.02-12.09,5.02H17.11c-4.71,0-8.99-1.92-12.09-5.02c-3.1-3.1-5.02-7.38-5.02-12.09V17.19 C0,12.48,1.92,8.2,5.02,5.1C8.12,2,12.4,0.08,17.11,0.08h32.98c3.19,0,5.78,2.59,5.78,5.78c0,3.19-2.59,5.78-5.78,5.78H17.11 c-1.52,0-2.9,0.63-3.91,1.63c-1.01,1.01-1.63,2.39-1.63,3.91v88.58c0,1.52,0.63,2.9,1.63,3.91c1.01,1.01,2.39,1.63,3.91,1.63h87.95 c1.52,0,2.9-0.63,3.91-1.63s1.63-2.39,1.63-3.91V72.58L110.6,72.58z M112.42,17.46L54.01,76.6c-2.23,2.27-5.89,2.3-8.16,0.07 c-2.27-2.23-2.3-5.89-0.07-8.16l56.16-56.87H78.56c-3.19,0-5.78-2.59-5.78-5.78c0-3.19,2.59-5.78,5.78-5.78h26.5 c5.12,0,11.72-0.87,15.65,3.1c2.48,2.51,1.93,22.52,1.61,34.11c-0.08,3-0.15,5.29-0.15,6.93c0,3.19-2.59,5.78-5.78,5.78 c-3.19,0-5.78-2.59-5.78-5.78c0-0.31,0.08-3.32,0.19-7.24C110.96,30.94,111.93,22.94,112.42,17.46L112.42,17.46z" />
+                      </g>
+                    </svg>
+                  </Link>
                 </div>
                 <ResponsiveContainer
                   className={styles.chart}
@@ -760,7 +805,7 @@ export default function Position({
                       outerRadius={140}
                       fill="#8884d8"
                       paddingAngle={0}
-                      dataKey="balance"
+                      dataKey="value"
                       style={{ cursor: "pointer", outline: "none" }}
                     >
                       <Label
@@ -858,7 +903,26 @@ export default function Position({
                       <span>Position Ratio</span>
                     </div>
                     <div className={styles.col}>
-                      <span>High Correlation With</span>
+                      <Tooltip
+                        label={
+                          "'NA' means token doesn't have high correlation with any other tokens."
+                        }
+                        position="top"
+                        withArrow
+                        classNames={{
+                          tooltip: styles.tooltip,
+                        }}
+                      >
+                        <span>
+                          High Correlation With
+                          <svg
+                            viewBox="0 0 24 24"
+                            data-testid="InfoOutlinedIcon"
+                          >
+                            <path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8"></path>
+                          </svg>
+                        </span>
+                      </Tooltip>
                     </div>
                     <div className={styles.col}>
                       <span>Liquidation Fee</span>
@@ -946,7 +1010,26 @@ export default function Position({
                         <span>Position Ratio</span>
                       </div>
                       <div className={styles.col}>
-                        <span>High Correlation With</span>
+                        <Tooltip
+                          label={
+                            "'NA' means token doesn't have high correlation with any other tokens."
+                          }
+                          position="top"
+                          withArrow
+                          classNames={{
+                            tooltip: styles.tooltip,
+                          }}
+                        >
+                          <span>
+                            High Correlation With
+                            <svg
+                              viewBox="0 0 24 24"
+                              data-testid="InfoOutlinedIcon"
+                            >
+                              <path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8"></path>
+                            </svg>
+                          </span>
+                        </Tooltip>
                       </div>
                       <div className={styles.col}></div>
                     </div>
